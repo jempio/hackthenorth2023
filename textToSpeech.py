9ac213da04239b496f309460e922b0d59eadcb4a
jli <nitsujil3002@gmail.com>
2023-09-17 01:46:10 -0400

    text to speech

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,22 +1,14 @@
 from gtts import gTTS
-from pydub import AudioSegment
-import os
+from tempfile import NamedTemporaryFile
+from playsound import playsound
 
-# Text you want to convert to speech
-text = "Hello, this is a sample text-to-speech conversion."
+def generateSpeech(text):
 
-# Create a gTTS object
-tts = gTTS(text)
+    # Create a gTTS object
+    tts = gTTS(text)
 
-# Save the speech as an MP3 file
-tts.save("output.mp3")
+    # Save the speech as an audio file
+    tts.save("output.mp3") 
 
-# Convert the MP3 file to WAV format
-audio = AudioSegment.from_mp3("output.mp3")
-audio.export("output.wav", format="wav")
+    playsound("output.mp3")
 
-# Clean up by removing the temporary audio files
-os.remove("output.mp3")
-
-# Play the audio file if needed
-os.system("aplay output.wav")  # You may need to install aplay or use a different player
